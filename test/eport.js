0292cdcc37c0a9934ac80098facdfd0be92f7887
luozhihua <luozhihua@21tb.com>
2014-11-05 23:45:04 +0800

    Add test case.

--- test/eport.js
+++ test/eport.js
@@ -1,4 +1,4 @@
-var freeport = require('eport');
+var eport = require('eport');
 var assert = require('assert');
 
 exports.sanity = function(done) {
@@ -9,5 +9,24 @@ exports.sanity = function(done) {
     assert(port !== 0);
     done();
   });
+};
 
+exports.sanity2 = function(done) {
+  eport('0.0.0.0', function(error, port) {
+
+    assert(error === null);
+    assert(typeof port === 'number');
+    assert(port !== 0);
+    done();
+  });
+};
+
+exports.sanity3 = function(done) {
+  eport('localhost', function(error, port) {
+
+    assert(error === null);
+    assert(typeof port === 'number');
+    assert(port !== 0);
+    done();
+  });
 };
